86c3bf813d6280b9206345e55ccd634efe3a6cd6
sayan bhattacharjee <sayanbhattacharya0001@gmail.com>
2025-11-10 20:22:38 +0530

    feat: enhance signup process with welcome email and improve signin error handling

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../../backend/models/user.model.js";
 import bcryptjs from "bcryptjs";
 import {generateTokenAndSetCookie} from "../../backend/utils/generateTokenAndSetCookie.js";
 import crypto from "crypto";
-import {sendVerificationEmail} from "../../backend/mailtrap/emails.js";
+import {sendVerificationEmail, sendWelcomeEmail} from "../../backend/mailtrap/emails.js";
 
 export const signup = async (req, res) => {
     const {email,name,password} = req.body;
@@ -48,9 +48,70 @@ export const signup = async (req, res) => {
     }
 }
 export const signin = async (req, res) => {
-    res.send("signin");
+    const {email,password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({success:false,message:"User not found"})
+        }
+
+        const isPasswordCorrect  = await bcryptjs.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(401).json({success:false,message:"Invalid credentials"})
+        }
+
+        generateTokenAndSetCookie(res,user._id);
+
+        user.lastLogin = new Date();
+        await user.save();
+
+        res
+            .status(200)
+            .json({
+                success:true,
+                message:"User signed in successfully",
+                user:{
+                    ...user._doc,
+                    password:undefined
+                }});
+    } catch (error) {
+        console.log("Error in signin: ",error);
+    }
 }
 export const logout = async (req, res) => {
-    res.send("logout");
+    res.clearCookie("token");
+    res.status(200).json({success:true,message:"User logged out successfully"})
 }
 
+export const verifyEmail = async (req, res) => {
+    const { code } = req.body;
+
+    try {
+        
+        const user = await User.findOne({verificationToken:code});
+        if(!user){
+            return res.status(404).json({success:false,message:"User not found"})
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationExpiresAt = undefined;
+        await user.save();
+
+
+        await sendWelcomeEmail(user.email,user.name);
+
+        res
+            .status(200)
+            .json({
+                success:true,
+                message:"User verified successfully",
+                user:{
+                    ...user._doc,
+                    password:undefined
+                }});
+    } catch (error) {
+        
+    }
+}
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,30 +1,50 @@
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 import { VERIFICATION_EMAIL_TEMPLATE } from "./email.template.js";
 
-export const sendVerificationEmail = async (email,verificationToken) => {
-    const recipient = [{email}];
+export const sendVerificationEmail = async (email, verificationToken) => {
+    const recipient = [{ email }];
 
     try {
-
         const response = await mailtrapClient.send({
             from: sender,
             to: recipient,
             subject: "Verify Your Email",
-            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}",verificationToken),
+            html: VERIFICATION_EMAIL_TEMPLATE.replace(
+                "{verificationCode}",
+                verificationToken
+            ),
             category: "Email Verification",
         });
 
         console.log("Email sent successfully:", response);
-        
     } catch (error) {
         console.error("Error sending email:", error);
-        throw new Error(`Failed to send verification email: ${error.message}`)
+        throw new Error(`Failed to send verification email: ${error.message}`);
     }
-}
+};
 
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
 
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "515b4e3c-d170-4ef2-ab47-d4a37249ebf0",
+            template_variables: {
+                user_name: name,
+                next_step_link: "Test_Next_step_link",
+                get_started_link: "Test_Get_started_link",
+                onboarding_video_link: "Test_Onboarding_video_link",
+            },
+        });
 
-
+        console.log("Welcome Email sent successfully:", response);
+    } catch (error) {
+        console.error("Error sending Welcome email:", error);
+        throw new Error(`Failed to send Welcome email: ${error.message}`);
+    }
+};
 
 // client
 //   .send({
